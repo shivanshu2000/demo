c257030ca58eefaf99a1a46fcb4da2e7fffdf2c3
Shivanshu2000 <shivanshusr82@gmail.com>
2021-03-08 18:37:07 +0530

    not a good day

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const express = require("express");
-const mongoose = require("mongoose");
-const flash = require("connect-flash");
-const session = require("express-session");
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const express = require('express');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
 const app = express();
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
   })
 );
 
 app.use(flash());
 
 app.use(function (req, res, next) {
-  res.locals.error = req.flash("error");
+  res.locals.error = req.flash('error');
   next();
 });
 
-const routes = require("./routes/index");
+const routes = require('./routes/index');
 
 app.use(routes);
 
@@ -41,7 +41,9 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB connection successful!"));
-
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => {
+    console.log(err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port);
